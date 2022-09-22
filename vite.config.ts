ebcc6c225391ab157408cfc10dfd01df62a7f51b
WOOGUGU <WOOGUGU@outlook.com>
2022-09-22 11:00:34 +0800

    添加路径别名 `@`

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // 路径别名
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
   css: {
     // css预处理器
     preprocessorOptions: {
@@ -13,4 +21,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
